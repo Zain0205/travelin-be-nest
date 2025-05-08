48e07b67b66abc13199668ddcc160fbb36ac2d5c
Fahmi Zain <fahmizain000@gmail.com>
2025-05-08 20:02:09 +0700

    Create file-upload.interceptor.ts

---
+++ src/common/file-upload.interceptor.ts
@@ -0,0 +1,36 @@
+import {
+  Injectable,
+  NestInterceptor,
+  Type,
+  mixin,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
+import { multerOptions } from './utils/multer.utils';
+
+interface FieldConfig {
+  name: string;
+  maxCount?: number;
+}
+
+// Interceptor untuk multiple fields
+export function FileFieldsUploadInterceptor(
+  fields: FieldConfig[],
+): Type<NestInterceptor> {
+  @Injectable()
+  class Interceptor implements NestInterceptor {
+    private readonly interceptor: NestInterceptor;
+
+    constructor() {
+      const targetClass = FileFieldsInterceptor(fields, multerOptions);
+      this.interceptor = new targetClass();
+    }
+
+    intercept(context: ExecutionContext, next: CallHandler) {
+      return this.interceptor.intercept(context, next);
+    }
+  }
+
+  return mixin(Interceptor);
+}
