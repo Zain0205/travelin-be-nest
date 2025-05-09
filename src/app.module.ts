23115eddb7ee39b0c0549711b423c4c4a3fcb841
Fahmi Zain <fahmizain000@gmail.com>
2025-05-09 13:57:25 +0700

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CommonModule } from './common/common.module';
 import { UserModule } from './user/user.module';
+import { TravelPackageModule } from './travel-package/travel-package.module';
 
 @Module({
-  imports: [CommonModule, UserModule],
+  imports: [CommonModule, UserModule, TravelPackageModule],
   controllers: [],
   providers: [],
 })
