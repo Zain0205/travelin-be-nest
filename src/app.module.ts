2afdc5578359ae489584d776a95e2f0c6598dcf6
Fahmi Zain <fahmizain000@gmail.com>
2025-05-13 13:05:40 +0700

    add hotel module and flight module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { CommonModule } from './common/common.module';
 import { UserModule } from './user/user.module';
 import { TravelPackageModule } from './travel-package/travel-package.module';
+import { HotelModule } from './hotel/hotel.module';
+import { FlightModule } from './flight/flight.module';
 
 @Module({
-  imports: [CommonModule, UserModule, TravelPackageModule],
+  imports: [CommonModule, UserModule, TravelPackageModule, HotelModule, FlightModule],
   controllers: [],
   providers: [],
 })
