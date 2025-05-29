3c48c27397afaa6f72d2b69a07a5a31c0996e1ad
Fahmi Zain <fahmizain000@gmail.com>
2025-05-29 17:59:58 +0700

    Create booking.model.ts

---
+++ src/model/booking.model.ts
@@ -0,0 +1,69 @@
+import { BookingStatus, BookingType, PaymentMethod, PaymentStatus } from '@prisma/client';
+
+interface BookingBase {
+  userId: number;
+  travelDate: Date;
+  type: BookingType;
+}
+
+export interface PackageBooking extends BookingBase {
+  packageId: number;
+  type: typeof BookingType.package;
+}
+
+export interface HotelBookingItem {
+  hotelId: number;
+  checkInDate: Date;
+  checkOutDate: Date;
+  nights: number;
+}
+
+export interface HotelBooking extends BookingBase {
+  type: typeof BookingType.hotel;
+  hotels: HotelBookingItem[];
+}
+
+export interface FlightBookingItem {
+  flightId: number;
+  passengerName: string;
+  seatClass: string;
+}
+
+export interface FlightBooking extends BookingBase {
+  type: typeof BookingType.flight;
+  flights: FlightBookingItem[];
+}
+
+export interface CustomBooking extends BookingBase {
+  type: typeof BookingType.custom;
+  hotels?: HotelBookingItem[];
+  flights?: FlightBookingItem[];
+}
+
+export type BookingInput =
+  | PackageBooking
+  | HotelBooking
+  | FlightBooking
+  | CustomBooking;
+
+export interface PaymentInput {
+  bookingId: number;
+  method: PaymentMethod;
+  amount: number;
+  proofUrl?: string;
+}
+
+export interface GetBookingsQuery {
+  status?: BookingStatus;
+  paymentStatus?: PaymentStatus;
+  type?: BookingType;
+  startDate?: Date;
+  endDate?: Date;
+  page?: number;   
+  limit?: number;  
+}
+
+export interface RescheduleInput {
+  bookingId: number;
+  requestedDate: Date;
+}
