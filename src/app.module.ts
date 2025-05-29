d38f51b3139e7ac464ae2fc8267797bd6c91ca51
Fahmi Zain <fahmizain000@gmail.com>
2025-05-29 18:00:45 +0700

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { UserModule } from './user/user.module';
 import { TravelPackageModule } from './travel-package/travel-package.module';
 import { HotelModule } from './hotel/hotel.module';
 import { FlightModule } from './flight/flight.module';
+import { BookingModule } from './booking/booking.module';
+import { PaymentModule } from './payment/payment.module';
 
 @Module({
-  imports: [CommonModule, UserModule, TravelPackageModule, HotelModule, FlightModule],
+  imports: [CommonModule, UserModule, TravelPackageModule, HotelModule, FlightModule, BookingModule, PaymentModule],
   controllers: [],
   providers: [],
 })
