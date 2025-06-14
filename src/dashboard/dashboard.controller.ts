3f1f1dfd80e4710e1be8c0e2f8c95b9d33d4331d
Fahmi Zain <fahmizain000@gmail.com>
2025-06-14 20:10:38 +0700

    Create dashboard.controller.ts

---
+++ src/dashboard/dashboard.controller.ts
@@ -0,0 +1,74 @@
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { DashboardService } from './dashboard.service';
+import { ValidationService } from 'src/common/validation.service';
+import { Roles } from 'src/user/decorator/roles.decorator';
+import { JwtAuthGuard } from 'src/user/jwt-auth.guard';
+import { RolesGuard } from 'src/user/roles.guard';
+import { CurrentUser } from 'src/user/decorator/current-user.decorator';
+import { MonthlyReportQuery, MonthlyReportQuerySchema } from './dashboard.validation';
+
+@Controller('/api/dashboard')
+@UseGuards(JwtAuthGuard, RolesGuard)
+export class DashboardController {
+  constructor(
+    private dashboardService: DashboardService,
+    private validationService: ValidationService
+  ) { }
+
+  @Get('/agent/statistics')
+  @Roles('agent')
+  async getAgentStatistics(@CurrentUser() user: any) {
+    return this.dashboardService.getAgentStatistic(user.id);
+  }
+
+  @Get('/agent/monthly-report')
+  @Roles('agent')
+  async getAgentMonthlyReport(
+    @CurrentUser() user: any,
+    @Query() query: MonthlyReportQuery
+  ) {
+    const validatedQuery = this.validationService.validate(
+      MonthlyReportQuerySchema as any,
+      query
+    ) as MonthlyReportQuery
+
+    return this.dashboardService.getAgentMonthlyReport(
+      user.id,
+      validatedQuery.year,
+      validatedQuery.month
+    );
+  }
+
+  @Get('/agent/packages')
+  @Roles('agent')
+  async getAgentPackages(@CurrentUser() user: any) {
+    return this.dashboardService.getAgentPackages(user.id);
+  }
+
+  @Get('/admin/statistics')
+  @Roles('admin')
+  async getAdminStatistics() {
+    return this.dashboardService.getAdminStatistics();
+  }
+
+  @Get('/admin/monthly-report')
+  @Roles('admin')
+  async getAdminMonthlyReport(@Query() query: any) {
+    const validatedQuery = this.validationService.validate(
+      MonthlyReportQuerySchema as any,
+      query
+    ) as MonthlyReportQuery;
+
+    return this.dashboardService.getAdminMonthlyReport(
+      validatedQuery.year,
+      validatedQuery.month
+    );
+  }
+
+  @Get('/admin/agent-performances')
+  @Roles('admin')
+  async getAgentPerformances() {
+    return this.dashboardService.getAgentPerformances();
+  }
+
+}
