23d01a10a2d717bbd16fb438584f9359f8887e8e
Fahmi Zain <fahmizain000@gmail.com>
2025-07-04 16:45:08 +0700

    Create user.module.ts

---
+++ src/reviews/user.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { UserController } from '../user/user.controller';
+import { UserService } from '../user/user.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_ACCESS_EXPIRATION', '15m'),
+        },
+      }),
+    }),
+  ],
+  controllers: [UserController],
+  providers: [UserService],
+  exports: [UserService],
+})
+export class UserModule {}
